261a1fb045bb2d3bc5fc5cfe7b4f9fe0295df971
kais <kk@onlfx.com>
2020-11-18 03:12:06 -0600

    initial app structure .idea config files cli, csv dependencies cli arg processing & help

---
+++ src/main/java/com/ces/ProcessCliArgs.java
@@ -0,0 +1,88 @@
+package com.ces;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.ParseException;
+
+public class ProcessCliArgs {
+
+    public void run(String[] args) {
+        CommandLine commandLine = parseArguments(args);
+    }
+
+    private CommandLine parseArguments(String[] args) {
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine commandLine = null;
+        Options options = cliOptions();
+
+        try {
+            commandLine = commandLineParser.parse(options, args);
+
+            if (commandLine.hasOption("help") || commandLine.hasOption("version")) {
+                cliHelp();
+
+                System.exit(0);
+            }
+        } catch (MissingOptionException | MissingArgumentException e) {
+            System.err.println(e.getMessage());
+            cliHelp();
+
+            System.exit(1);
+        } catch (ParseException e) {
+            System.err.println("Unknown Error. Failed to process cli arguments.");
+            System.err.println(e.toString());
+            cliHelp();
+
+            System.exit(1);
+        }
+
+        return commandLine;
+    }
+
+    private Options cliOptions() {
+        Options options = new Options();
+        OptionGroup optionGroup = new OptionGroup();
+
+        optionGroup.addOption(
+                Option.builder("f")
+                        .longOpt("file")
+                        .desc("Full path to CSV file to parse.")
+                        .hasArg()
+                        .argName("FILE")
+                        .build()
+        );
+        optionGroup.addOption(
+                Option.builder("h")
+                        .longOpt("help")
+                        .desc("Display this help message.")
+                        .build()
+        );
+        optionGroup.addOption(
+                Option.builder("v")
+                        .longOpt("version")
+                        .desc("Display the application version.")
+                        .build()
+        );
+
+        optionGroup.setRequired(true);
+        options.addOptionGroup(optionGroup);
+
+        return options;
+    }
+
+    private void cliHelp() {
+        Options options = cliOptions();
+        HelpFormatter helpFormatter = new HelpFormatter();
+
+        String header = "Java App to parse a Product CSV file and output JSON.\n\nOptions:";
+        String footer = "\nVersion: 1.0\nSource & Docs: https://github.com/obsirdian/product-parser";
+        helpFormatter.printHelp("ProductParser", header, options, footer, true);
+    }
+}
---
+++ src/main/java/com/ces/ProductParser.java
@@ -0,0 +1,9 @@
+package com.ces;
+
+public class ProductParser {
+
+    public static void main(String[] args) {
+        ProcessCliArgs processCliArgs = new ProcessCliArgs();
+        processCliArgs.run(args);
+    }
+}
